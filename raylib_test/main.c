1ee946bda19240de22829a43183ffde703eb01c0
yuvalzim <126091309+yuvalzim@users.noreply.github.com>
2024-08-20 21:51:03 +0300

    Add files via upload

--- raylib_test/main.c
+++ raylib_test/main.c
@@ -1,31 +1,45 @@
 #include <stdio.h>
 #include "include/raylib.h"
 
+const int screenWidth = 800;
+const int screenHeight = 450;
+
+struct player
+{
+    int x, y;
+    double verticalVelocity, verticalAcceleration;
+    bool isRight;
+    Texture2D image;
+};
+
+struct player initializePlayer(int playerStartY, Texture2D playerImage)
+{
+    struct player Player;
+    Player.x = 0;
+    Player.y = playerStartY;
+    Player.image = playerImage;
+    return Player;
+}
+
 int main(void)
 {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
     
-    
-
     InitWindow(screenWidth, screenHeight, "Game");
 
     Texture2D backGroundImage = LoadTexture("bg.png");
-    Texture2D playerImage = LoadTexture("WALK.png");    
+    Texture2D playerImage = LoadTexture("WALK.png");
 
-    float frameWidth = (float)(playerImage.width / 8);
-    int maxFrames = (int)(playerImage.width / (int)frameWidth);
 
     const int playerStartY = screenHeight - playerImage.height/1.15;
-    int playerX = 0;
-    int playerY = playerStartY;
-    double playerVerticalVelocity = 0;
-    double playerVerticalAcceleration = 0;
+
+    struct player Player = initializePlayer(playerStartY, playerImage);
+    
+    float frameWidth = (float)(playerImage.width / 8);
+    int maxFrames = (int)(playerImage.width / (int)frameWidth);
 
     float timer = 0.0f;
     int frame = 0;
 
-    bool isRight = true;
     float current_frame_width = frameWidth;
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
@@ -33,22 +47,22 @@ int main(void)
     {
         timer += GetFrameTime();
 
-        if (!isRight && current_frame_width > 0){
+        if (!Player.isRight && current_frame_width > 0){
             current_frame_width = -1 * frameWidth;
         }
-        if (isRight && current_frame_width < 0){
+        if (Player.isRight && current_frame_width < 0){
             current_frame_width = frameWidth;
         }
 
         if (IsKeyDown(KEY_D))
         {
-            isRight = true;
-            playerX += 3;   
+            Player.isRight = true;
+            Player.x += 3;   
         }
         if (IsKeyDown(KEY_A))
         {
-            isRight = false;
-            playerX -= 3;
+            Player.isRight = false;
+            Player.x -= 3;
         }
         if (IsKeyDown(KEY_D) || IsKeyDown(KEY_A))
         {
@@ -64,19 +78,19 @@ int main(void)
 
         if (IsKeyPressed(KEY_SPACE))
         {
-            playerVerticalVelocity = -7.5;
-            playerVerticalAcceleration = 0.25;
+            Player.verticalVelocity = -7.5;
+            Player.verticalAcceleration = 0.25;
         }
         
         
 
-        playerY += (int)playerVerticalVelocity;
-        playerVerticalVelocity += playerVerticalAcceleration;
+        Player.y += (int)Player.verticalVelocity;
+        Player.verticalVelocity += Player.verticalAcceleration;
 
-        if (playerY >= playerStartY){
-            playerY = playerStartY;
-            playerVerticalVelocity = 0;
-            playerVerticalAcceleration = 0;
+        if (Player.y >= playerStartY){
+            Player.y = playerStartY;
+            Player.verticalVelocity = 0;
+            Player.verticalAcceleration = 0;
         }
 
         BeginDrawing();
@@ -88,7 +102,7 @@ int main(void)
             DrawTextureRec(
                 playerImage,
                 (Rectangle){frameWidth * frame, 0, current_frame_width, (float)playerImage.height},
-                (Vector2){playerX, playerY},
+                (Vector2){Player.x, Player.y},
                 RAYWHITE);
 
         EndDrawing();
